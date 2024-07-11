b0348c8d167fbf5727ef4e0455869e01f356dc16
Maggie Holman <maggie@baycast.com>
2024-07-11 13:56:08 -0400

    Homepage updates

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('resume').addEventListener('click', function() {
+    window.location.href = 'images/TechnicalFallResume.pdf'; // Path to your resume file
+});
+
+document.getElementById('artportfolio').addEventListener('click', function() {
+    window.location.href = 'https://maggieholman.github.io/ArtPortfolio/'; // Path to your resume file
+});
