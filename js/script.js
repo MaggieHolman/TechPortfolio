5bf6a0d83ac42b4ba7ac80c8cd70df9da5a26948
Maggie Holman <maggie@baycast.com>
2024-07-29 09:22:09 -0400

    Overall update to site

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ document.getElementById('resume').addEventListener('click', function() {
 document.getElementById('artportfolio').addEventListener('click', function() {
     window.location.href = 'https://maggieholman.github.io/ArtPortfolio/'; // Path to your resume file
 });
+
+document.getElementById('aboutme').addEventListener('click', function() {
+    window.location.href = 'about.html'; // Path to your resume file
+});
+
+document.getElementById('projects').addEventListener('click', function() {
+    window.location.href = 'projects.html'; // Path to your resume file
+});
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/maggie-holman-3421b226a/'; // Path to your resume file
+});
+document.getElementById('github').addEventListener('click', function() {
+    window.location.href = 'https://github.com/MaggieHolman'; // Path to your resume file
+});
+document.getElementById('email').addEventListener('click', function() {
+    window.location.href = 'projects.html'; // Path to your resume file
+});
+document.getElementById('instagram').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/maggiesarttt/'; // Path to your resume file
+});
